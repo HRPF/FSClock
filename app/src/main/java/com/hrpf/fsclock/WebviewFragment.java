6580e382c52fc7360120b568660971dd1fae21d2
HR <2379457118@qq.com>
2023-09-10 15:53:09 +0800

    Add:Bring up Web View

---
+++ app/src/main/java/com/hrpf/fsclock/WebviewFragment.java
@@ -0,0 +1,81 @@
+package com.hrpf.fsclock;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Use the {@link WebviewFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class WebviewFragment extends Fragment {
+
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private String mParam1;
+    private String mParam2;
+
+    public WebviewFragment() {
+        // Required empty public constructor
+    }
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment WebviewFragment.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static WebviewFragment newInstance(String param1, String param2) {
+        WebviewFragment fragment = new WebviewFragment();
+        Bundle args = new Bundle();
+        args.putString(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getString(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootview = inflater.inflate(R.layout.fragment_webview, container, false);
+        WebView webView = rootview.findViewById(R.id.remote_sensor);
+        // 直接通过WebView显示网页
+        webView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                //使用WebView加载显示url
+                view.loadUrl(url);
+                //返回true
+                return true;
+            }
+        });
+        webView.loadUrl("https://cn.bing.com/");
+        // 启用javascript
+        webView.getSettings().setJavaScriptEnabled(true);  //设置WebView属性,运行执行js脚本
+        return rootview;
+    }
+}
