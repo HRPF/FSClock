d6ba85c4fde5aeb63f5e48b6ed63d692f3078353
Huang Rui <rui.huang@joynext.com>
2023-10-13 16:26:39 +0800

    Add: 开机自启设置

--- app/src/main/java/com/hrpf/fsclock/StartReceiver.java
+++ app/src/main/java/com/hrpf/fsclock/StartReceiver.java
@@ -2,6 +2,10 @@ package com.hrpf.fsclock;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+
+import androidx.preference.PreferenceManager;
+
 public class StartReceiver extends BroadcastReceiver {
     public StartReceiver() {
     }
@@ -10,10 +14,16 @@ public class StartReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         //此处及是重启的之后，打开我们app的方法
         if ("android.intent.action.BOOT_COMPLETED".equals(intent.getAction())) {
-            Intent bootIntent= new Intent(context, ViewPager.class);
-            bootIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);//非常重要，如果缺少的话，程序将在启动时报错
-            //自启动APP（Activity）
-            context.startActivity(bootIntent);
+            SharedPreferences appPreferences;
+            appPreferences = PreferenceManager.getDefaultSharedPreferences(context.getApplicationContext());
+            boolean start_at_boot = appPreferences.getBoolean("start_at_boot", false);
+            if (start_at_boot){
+                Intent bootIntent= new Intent(context, ViewPager.class);
+                //新活动会成为历史栈中的新任务（一组活动）的开始,通常用于具有"launcher"行为的活动
+                bootIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                //自启动APP（Activity）
+                context.startActivity(bootIntent);
+            }
         }
     }
 }
